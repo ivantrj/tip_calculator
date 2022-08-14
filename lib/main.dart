512dd5d9559c0b007c975d24ece1530a85d97b8d
Ivan Trj <ivantrajanovski@gmail.com>
2022-08-14 08:43:02 +0200

    small fixes

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,22 +40,32 @@ class _MyHomePageState extends State<MyHomePage> {
         home: Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Padding(
               padding: const EdgeInsets.all(20),
               child: Text(
-                tip,
-                style: const TextStyle(fontSize: 30),
+                'Your tip is $tip',
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
             ),
-            const Text('Total Amount'),
+            const Text(
+              'Bill Total',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+            ),
             SizedBox(
-              width: 70,
+              width: 100.0,
               child: TextField(
+                style:
+                    const TextStyle(fontWeight: FontWeight.w600, fontSize: 25),
                 controller: controller,
                 textAlign: TextAlign.center,
-                decoration: const InputDecoration(hintText: '\$100.00'),
+                // decoration: const InputDecoration(hintText: '\$0.00'),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '\$0.00',
+                ),
                 keyboardType:
                     const TextInputType.numberWithOptions(decimal: true),
               ),
@@ -68,12 +77,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   onPressed: updateSelection,
                   children: const [Text('10%'), Text('15%'), Text('20%')]),
             ),
-            TextButton(
+            ElevatedButton(
               onPressed: calculateTip,
-              style: TextButton.styleFrom(
-                primary: Colors.green,
-                backgroundColor: Colors.white,
-              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, elevation: 10.0),
               child: const Text('Calculate Tip'),
             ),
           ],
